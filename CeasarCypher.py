34b8b47c3fae8e878ddc0269687c823164c28fd0
BOBOC <bogdan.boboc@capgemini.com>
2022-10-11 13:51:42 +0300

    Solves Ceasar's cyphr withouth translation, Exercise 3.

--- CeasarCypher.py
+++ CeasarCypher.py
@@ -7,10 +7,21 @@ def caesar(plain_text, shift_num=1):
     trantab = str.maketrans(letters, mask)
     return plain_text.translate(trantab)
 
+def caesarNoTranslate(plain_text, shift_num=1):
+    letters = string.ascii_lowercase
+    newText = ""
+    for letter in plain_text:
+        if letter in letters:
+            letter=letters[(letters.index(letter)+shift_num)%len(letters)]
+        newText+=letter
+    return newText
+
 
 def main():
     value = input("Please enter the message to cypher:\n")
     positionsToShift=int(input("Please enter how many positions to shift the characters:\n"))
+    newVal = caesarNoTranslate(value,positionsToShift)
+    print("The encrypted message withouth translation is: ",newVal)
     newVal = caesar(value,positionsToShift)
     print("The encrypted message is: ",newVal)
 
